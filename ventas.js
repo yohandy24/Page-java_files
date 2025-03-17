9d0dc052304a2dc96757d01159c16614e97c4420
yohandy24 <yohandygutierrez@gmail.com>
2025-03-17 16:06:53 -0400

    comit ventas

--- ventas.js
+++ ventas.js
@@ -2,19 +2,20 @@ fetch("productos.json")
 .then(responsive => responsive.json())
 .then(data => {
     
-    
+
     const  foto = document.querySelector(".foto")
     const  urlParams = new URLSearchParams(window.location.search)
     const  id = parseInt( urlParams.get("id"));
+    const desamoni = data.monitores.find(item =>item.id === id)
     const imgt__1 = document.createElement("div")
     imgt__1.classList.add(`imgt__1`)
-    imgt__1.innerHTML = `<img src="/imagenes/monitor ${id}.jpeg">`
+    imgt__1.innerHTML = `<img src="${desamoni.photo}">`
     foto.appendChild(imgt__1)
     // esta es la parte de la foto
 
 
 
-    const desamoni = data.monitores.find(item =>item.id === id)
+
     const descripción = document.querySelector(".descripción")
     const desa = document.createElement("div")
     desa.classList.add(`desa`)
