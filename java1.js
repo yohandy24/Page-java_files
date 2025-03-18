d26b9f71cba260f5475702b96894c34d1321dff3
yohandy24 <yohandygutierrez@gmail.com>
2025-03-18 17:39:19 -0400

    version-2

--- java1.js
+++ java1.js
@@ -5,6 +5,7 @@ let div = document.querySelector(".superofertas-1");
 fetch("productos.json")
  .then(response => response.json())
  .then(data => {
+    
     const productosdiv = document.querySelector(".productos")
     data.monitores.forEach(producto => {
         const productodiv = document.createElement("div");
@@ -23,6 +24,12 @@ fetch("productos.json")
     });
  })
  .catch(error => console.error ("error puto",error))
+
+
+/*esto es para el nav responsive 770px*/ 
+
+const barra__770px = document.querySelector(".barra__770px")
+barra__770px.insertAdjacentHTML("beforeend",` <a href="" class=""><i class="fa-sharp fa-solid fa-cart-shopping carrito"></i></a>`),
 
 
 submit.addEventListener("click",()=>{
