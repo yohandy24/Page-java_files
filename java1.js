6f0118b94009927d7b017b8502d14d89a320b997
yohandy24 <yohandygutierrez@gmail.com>
2025-03-26 22:26:43 -0400

    footer

--- java1.js
+++ java1.js
@@ -1,29 +1,86 @@
 let barra = document.querySelector(".input");
 let submit = document.querySelector(".submit");
 let div = document.querySelector(".superofertas-1");
+const article = document.querySelector(".article")
+
 
 fetch("productos.json")
  .then(response => response.json())
  .then(data => {
-    
+
+
+    const buscador__1 = document.querySelector(".buscador-1")
     const productosdiv = document.querySelector(".productos")
-    data.monitores.forEach(producto => {
+        data.monitores.forEach(producto => {
         const productodiv = document.createElement("div");
         productodiv.classList.add('producto');
         productodiv.innerHTML = `<div class="producto__${producto.id}">
         <img src="${producto.photo}">
-        <p>nombre: ${producto.nombre}</p>
-        <p>precio: ${producto.precio}</p>
+        <p><b>Nombre:</b> ${producto.nombre}</p>
+        <p><b>Precio:</b> ${producto.precio}</p>
         <a href="producto.html?id=${producto.id}"><strong>comprar</a>
 
         </div>
         `;
         productosdiv.appendChild(productodiv);
 
+  
         
     });
- })
- .catch(error => console.error ("error puto",error))
+
+    function buscar(){
+
+        const escrito = barra.value.toLowerCase()
+        buscador__1.innerHTML = ""
+        const resultados = data.monitores.filter(item =>
+        item.nombre.toLowerCase().includes(escrito) ||
+        item.marca.toLowerCase().includes(escrito))
+
+        resultados.forEach(producto =>{
+
+        const productodiv = document.createElement("div");
+        productodiv.classList.add('producto');
+        productodiv.innerHTML = `<div class="producto__${producto.id}">
+        <img src="${producto.photo}">
+        <p><b>Nombre:</b> ${producto.nombre}</p>
+        <p><b>Precio:</b> ${producto.precio}</p>
+        <a href="producto.html?id=${producto.id}"><strong>comprar</a>
+
+        </div>
+        `;
+
+        buscador__1.appendChild(productodiv);
+        buscador__1.classList.add(`b-1`)
+    })
+        
+      
+    }
+    submit.addEventListener("click",buscar)
+
+    barra.addEventListener("keydown",function(event){
+        if(event.key === "Enter"){
+            buscar();
+        }
+       
+
+    })
+    article.addEventListener("click",()=>{
+        buscador__1.classList.remove(`b-1`)
+        buscador__1.innerHTML = ""
+
+
+    })
+})
+
+
+
+
+
+ 
+
+.catch(error => console.error ("error puto",error))
+
+ 
 
 
 /*esto es para el nav responsive 770px*/ 
@@ -31,24 +88,12 @@ fetch("productos.json")
 const barra__770px = document.querySelector(".barra__770px")
 
 
-submit.addEventListener("click",()=>{
-    if (barra.value.trim() == "monitor") {
-        alert("hola");
-        document.querySelector(".buscador-1").lastChild.remove()
-        let divs = div.cloneNode(true);
-        document.querySelector(".buscador-1").appendChild(divs);
-
-    }
-    else{
-        alert("pon algo mas")
-        document.querySelector(".buscador-1").lastChild.remove()
-    }
-})
 
 /*hacer cerrar el nav*/
 const nav__left = document.getElementById("nav__left")
 const cerrar = document.querySelector(".x")
 const bar = document.querySelector(".bar")
+const active__nav = document.querySelector("#nav__left.active")
 
 cerrar.addEventListener("click",()=> {
     nav__left.classList.remove("active")
@@ -60,5 +105,8 @@ bar.addEventListener("click",()=> {
 })
 
 
+active__nav.addEventListener("click",()=>{
+    nav__left.classList.remove("active")
 
+})
 
--- ventas.js
+++ ventas.js
@@ -1,7 +1,10 @@
 fetch("productos.json")
 .then(responsive => responsive.json())
 .then(data => {
-    
+    let barra = document.querySelector(".input");
+    let submit = document.querySelector(".submit");
+    const article = document.querySelector(".article")
+    const buscador__1 = document.querySelector(".buscador-1") 
     const  foto = document.querySelector(".foto")
     const  urlParams = new URLSearchParams(window.location.search)
     const  id = parseInt( urlParams.get("id"));
@@ -30,5 +33,63 @@ fetch("productos.json")
     const precio__1 = document.querySelector(".precio__1")
     precio__1.innerHTML = `RD$ ${desamoni.precio}`
 
+    function buscar(){
+
+        const escrito = barra.value.toLowerCase()
+        buscador__1.innerHTML = ""
+        const resultados = data.monitores.filter(item =>
+        item.nombre.toLowerCase().includes(escrito) ||
+        item.marca.toLowerCase().includes(escrito))
+
+        resultados.forEach(producto =>{
+
+        const productodiv = document.createElement("div");
+        productodiv.classList.add('producto');
+        productodiv.innerHTML = `<div class="producto__${producto.id}">
+        <img src="${producto.photo}">
+        <p><b>Nombre:</b> ${producto.nombre}</p>
+        <p><b>Precio:</b> ${producto.precio}</p>
+        <a href="producto.html?id=${producto.id}"><strong>comprar</a>
+
+        </div>
+        `;
+
+        buscador__1.appendChild(productodiv);
+        buscador__1.classList.add(`b-1`)
+    })
+        
+      
+    }
+    submit.addEventListener("click",buscar)
+
+    barra.addEventListener("keydown",function(event){
+        if(event.key === "Enter"){
+            buscar();
+        }
+       
+
+    })
+    article.addEventListener("click",()=>{
+        buscador__1.classList.remove(`b-1`)
+        buscador__1.innerHTML = ""
+
+
+    })
+
    
 })
+
+const nav__left = document.getElementById("nav__left")
+const cerrar = document.querySelector(".x")
+const bar = document.querySelector(".bar")
+
+cerrar.addEventListener("click",()=> {
+    nav__left.classList.remove("active")
+
+})
+
+bar.addEventListener("click",()=> {
+    nav__left.classList.add("active")
+})
+
+
